de118e0792b7265be7c366bfa22c4d0f6fdd717d
jelgerjansen <jelgerjansen@hotmail.com>
2021-03-22 11:36:40 +0100

    Added the following outputs: the original occupancy profile (resolution of 10 minutes, to perform checks if necessary) and 4 files containing metabolic data (QConMet_day, QRadMet_dag, QConMet_night and QRadMet_night)  with a resolution of 1 hour, calculated using the mean value of the previous hour. For the temperature setpoints, the default calculation to calculate a lower resolution is also changed to the average temperature. Furthermore, different body areas are added for the different occupants (since U12 doesn't have an occupancy profile, only the body surface of the occupant 'school' is different).

--- Corpus/feeder.py
+++ Corpus/feeder.py
@@ -64,23 +64,45 @@ class IDEAS_Feeder(object):
         dat=dict.fromkeys(variables.keys(),[])
 
         occ=[]
+        QMet=[]
+
+        # Define variables to calculate metabolic heat gains. Since only thermal comfort is studied, only sensible heat is calculated (no latent heat is considered here).
+        # Values taken from ASHRAE Handbook - Fundamentals (2009), body surface of 1.8 m^2 is assumed
+        QMetActivePerA=80 # seated (quiet)=60 (35%), standing (relaxed)=70 (35%), walking (slow)=115 (10%), cooking=95-115 (10%), housecleaning=115-200 (10%)
+        QMetSleepPerA=40
+        A = {'U12': 0.9, 'School': 1.68, 'Unemployed': 1.8, 'PTE': 1.8, 'FTE': 1.8, 'Retired': 1.8} # based on https://doi.org/10.1016/j.dib.2017.10.036 ! For U12, no occupancy is generated, so it is not taken into account for metabolic heat gains
+        radFra=0.5
 
         for i in range(self.nBui): # loop over all households
             #print(i)
             hou = cPickle.load(open(str(self.name)+'_'+str(i)+'.p','rb')) #load results
-
             var = np.array(hou.occ)
-            var_resolution = np.zeros((var.shape[0],8761))
+            QConMet_day = np.zeros((var.shape[0],var.shape[1]))
+            QRadMet_day = np.zeros((var.shape[0],var.shape[1]))
+            QConMet_night = np.zeros((var.shape[0],var.shape[1]))
+            QRadMet_night = np.zeros((var.shape[0],var.shape[1]))
             for member in range(0,var.shape[0]):
+                QConMet_day[member,:]=var[member,:]*((var[member,:]==1)*QMetActivePerA*A[str(hou.members[member])]*(1-radFra)+(var[member,:]!=1)*0)
+                QRadMet_day[member,:]=var[member,:]*((var[member,:]==1)*QMetActivePerA*A[str(hou.members[member])]*radFra+(var[member,:]!=1)*0)
+                QConMet_night[member,:]=var[member,:]*((var[member,:]==2)*QMetSleepPerA*A[str(hou.members[member])]*(1-radFra)+(var[member,:]!= 2)*0)
+                QRadMet_night[member,:]=var[member,:]*((var[member,:]==2)*QMetSleepPerA*A[str(hou.members[member])]*radFra+(var[member,:]!=2)*0)
+
+            QMetOneBuilding=np.array([QConMet_day,QRadMet_day,QConMet_night,QRadMet_night])
+            QMetOneBuilding_resolution=np.zeros((QMetOneBuilding.shape[0],8761))
+            QMetOneBuilding=QMetOneBuilding.sum(axis=1)
+
+            for list in range(0,QMetOneBuilding.shape[0]):
             # option 1: first point is the existing one for minute 0 (midnight), and the following are averages of previous 60 minutes.
-            # This is only meaningfull if the occupancy profiles are converted first to metabolic heat gains before averaging.
-                # var_resolution[member,:]=np.append(var[member,0],var[member,1:].reshape(-1,6).mean(axis=-1))  # moving average: mean of every 6 points
+            # This is only meaningful if the occupancy profiles are converted first to metabolic heat gains before averaging.
+                QMetOneBuilding_resolution[list]=np.append(QMetOneBuilding[list,0],QMetOneBuilding[list,1:].reshape(-1,6).mean(axis=-1))  # moving average: mean of every 6 points
             # option 2: the setpoint of the first 10 minutes is assumed to be constant for the entire hour (instead of using an average value)
-                var_resolution[member,:]=np.append(var[member,:var.shape[1]-1].reshape(-1,6)[:,0],var[member,var.shape[1]-1])
+                # var_resolution[member,:]=np.append(var[member,:var.shape[1]-1].reshape(-1,6)[:,0],var[member,var.shape[1]-1])
             if len(occ)!=0:
-                occ=np.vstack((occ,var_resolution))
+                occ=np.vstack((occ,var))
+                QMet=np.vstack((QMet,QMetOneBuilding_resolution))
             else:
-                occ=var_resolution
+                occ=var
+                QMet=QMetOneBuilding_resolution
 
             for variable in variables.keys(): #loop over variables
                 var = eval('hou.'+variable)
@@ -89,9 +111,9 @@ class IDEAS_Feeder(object):
                     var=np.append(var[0],var[1:].reshape(-1,60).mean(axis=-1)) # moving average: mean of every 60 points
                 if len(var)==52561: # if per 10 minute data, make it per 60 min (sh_day, sh_night, sh_bath)
                     # option 1: first point is the existing one for minute 0 (midnight), and the following are averages of previous 60 minutes
-                    var1 = np.append(var[0],var[1:].reshape(-1,6).mean(axis=-1))  # moving average: mean of every 6 points
+                    var = np.append(var[0],var[1:].reshape(-1,6).mean(axis=-1))  # moving average: mean of every 6 points
                     # option 2: the setpoint of the first 10 minutes is assumed to be constant for the entire hour (instead of using an average value).
-                    var = np.append(var[:len(var)-1].reshape(-1,6)[:,0],var[len(var)-1])
+                    # var = np.append(var[:len(var)-1].reshape(-1,6)[:,0],var[len(var)-1])
                 if variable in ['sh_day','sh_bath','sh_night'] and self.sh_K: # if space heating setting and Kelvin required
                     variables[variable]=variables[variable].replace("Celsius", "Kelvin") # change variable explanation
                     var=var+273.15 # make it in Kelvin
@@ -99,14 +121,6 @@ class IDEAS_Feeder(object):
                     dat[variable] = np.vstack((dat[variable],var)) # add new column
                 else: # if firts household (dat[variable] was empty)
                     dat[variable] = var # set equal to var
-
-            # Checks
-            # occupancy
-            sum=0
-            for i in range(0,occ.shape[1]):
-                if 1 in occ[:,i]:
-                    sum+=1
-            print(sum)
         
         #######################################################################
         # and output the array to txt
@@ -119,9 +133,24 @@ class IDEAS_Feeder(object):
             hea ='#1 \ndouble data('+str(int(data.shape[1]))+','+str(int(data.shape[0]))+') \n#' + variables[variable]
             np.savetxt(fname=variable+'.txt', X=data.T, header=hea,comments='', fmt='%.7g')
 
-        # save occupancy profiles
+        # save occupancy profiles and print as header the necessary for IDEAS, plus explanation for each variable
         tim = np.linspace(0, 31536000,occ.shape[1])
         data = np.vstack((tim,occ))
-        # print as header the necessary for IDEAS, plus explanation for each variable
-        hea = '#1 \ndouble data(' + str(int(data.shape[1])) + ',' + str(int(data.shape[0])) + ') \n#' + 'Occupancy profiles (absent=3, sleeping=2, present & active=1)'
+        hea = '#1 \ndouble data('+str(int(data.shape[1]))+','+str(int(data.shape[0]))+') \n#' + 'Occupancy profiles (absent=3, sleeping=2, present & active=1)'
         np.savetxt(fname='occupancy.txt', X=data.T, header=hea, comments='', fmt='%.7g')
+        tim = np.linspace(0, 31536000,QMet.shape[1])
+        data = np.vstack((tim,QMet[0,:]))
+        hea = '#1 \ndouble data('+str(int(data.shape[1]))+','+str(int(data.shape[0]))+') \n#' + 'Convective metabolic heat gains in the day zone in W'
+        np.savetxt(fname='QConMet_day.txt', X=data.T, header=hea, comments='', fmt='%.7g')
+        tim = np.linspace(0, 31536000,QMet.shape[1])
+        data = np.vstack((tim,QMet[1,:]))
+        hea = '#1 \ndouble data('+str(int(data.shape[1]))+','+str(int(data.shape[0]))+') \n#' + 'Radiative metabolic heat gains in the day zone in W'
+        np.savetxt(fname='QRadMet_day.txt', X=data.T, header=hea, comments='', fmt='%.7g')
+        tim = np.linspace(0, 31536000,QMet.shape[1])
+        data = np.vstack((tim,QMet[2,:]))
+        hea = '#1 \ndouble data('+str(int(data.shape[1]))+','+str(int(data.shape[0]))+') \n#' + 'Convective metabolic heat gains in the night zone in W'
+        np.savetxt(fname='QConMet_night.txt', X=data.T, header=hea, comments='', fmt='%.7g')
+        tim = np.linspace(0, 31536000,QMet.shape[1])
+        data = np.vstack((tim,QMet[3,:]))
+        hea = '#1 \ndouble data('+str(int(data.shape[1]))+','+str(int(data.shape[0]))+') \n#' + 'Radiative metabolic heat gains in the night zone in W'
+        np.savetxt(fname='QRadMet_night.txt', X=data.T, header=hea, comments='', fmt='%.7g')
